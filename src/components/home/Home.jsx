7c2e24351c6bf4bc3e67281c9e89fb214dff18a8
izaz-swe <izaz532963@gmail.com>
2023-04-06 03:18:51 +0600

    first

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Recent from './recent/Recent'
+export default function Home() {
+  return (
+    <Recent></Recent>
+  )
+}
---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../common/header/Header'
+import Home from '../home/Home'
+
+
+export const Pages = () => {
+  return (
+    <>
+        <Header></Header>
+        <Home></Home>
+    </>
+  )
+}
